019b97cd6fa6bff62141f610ed524244e672ecd1
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-04-03 16:20:15 +0700

    update 420 03/04

---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+public class DoorTrigger : MonoBehaviour
+{
+	[SerializeField]
+	public GameObject door;
+	
+	bool isOpened = false;
+	
+	void OntriggerEnter(Collider col)
+	{
+		if(!isOpened)
+		{
+			isOpened = true;
+			door.transform.position += new Vector3 (0,4,0);
+		}
+    }
+
+}
